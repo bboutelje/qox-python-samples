82d333cc1e0f3cfa05162941c6312ebd950d3393
Bruce Boutelje <bruceboutelje@hotmail.com>
2026-04-06 14:21:06 +0100

    jupytext experiment

--- examples/fdm_vs_analytic.py
+++ examples/fdm_vs_analytic.py
@@ -1,41 +1,45 @@
-import sys
-
-if "google.colab" in sys.modules:
-    # This runs only in Colab
-    import subprocess
-
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "qox", "-q"])
-
 import time
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import qox
 
+ny_tz = ZoneInfo("America/New_York")
+spot = 95.0
+strike = 100.0
+valuation_time = datetime(2025, 9, 25, 17, 0, tzinfo=ny_tz)
+expiry = datetime(2026, 9, 25, 17, 0, tzinfo=ny_tz)
+rate = 0.05
+vol = 0.2
+option_type = qox.OptionType.Call
+exercise_style = qox.ExerciseStyle.European
+
+
 # 1. Setup
-fdm_config = qox.FdmConfig(nodes=50000, time_steps=500, grid_std_devs=6.0)
+fdm_config = qox.FdmConfig(nodes=500, time_steps=5, grid_std_devs=6.0)
 config = qox.Config().add_policy(qox.InstrumentPolicy().european().fdm(fdm_config))
 
-expiry = datetime.now(timezone.utc) + timedelta(days=1)
-stock_option = qox.StockOption(
-    100.0, expiry, qox.OptionType.Call, qox.ExerciseStyle.European
-)
+# expiry = datetime.now(timezone.utc) + timedelta(days=1)
+stock_option = qox.StockOption(strike, expiry, option_type, exercise_style)
 
 market_frame = qox.OptionMarketFrame(
-    spot=100.0,
-    rate_curve=qox.RateCurve.continuous(0.05, qox.DayCountConvention.Act365Fixed),
-    vol_surface=qox.VolSurface.flat(0.2, qox.DayCountConvention.Act365Fixed),
+    spot=spot,
+    rate_curve=qox.RateCurve.continuous(rate, qox.DayCountConvention.Act365Fixed),
+    vol_surface=qox.VolSurface.flat(vol, qox.DayCountConvention.Act365Fixed),
 )
 
 # 2. Compute FDM
 start_time = time.perf_counter()
-fdm_result = stock_option.valuation().market(market_frame).config(config).compute()
+fdm_result = stock_option.valuation().at().market(market_frame).config(config).compute()
 duration = time.perf_counter() - start_time
 
 # 3. Compute Analytic
-analytic_result = stock_option.valuation().market(market_frame).compute()
+analytic_result = (
+    stock_option.valuation().at(valuation_time).market(market_frame).compute()
+)
 
 # 4. Print results
-print(f"Time taken: {duration * 1000:.4f} ms")
+print(f"Time taken: {duration * 1000000:.0f} \u03bcs")
 print(f"\n{'Metric':<12} | {'FDM':>15} | {'Analytic':>15} | {'% Dev':>15}")
 print("-" * 67)
 
